abb9db275684c80d6263e7d5a997d37505a80cb0
Alwyal <alwy2001@gmail.com>
2022-03-23 09:25:13 +0700

    second commit

---
+++ src/test/java/id/ac/uinsgd/alwyal/coba/CobaApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.uinsgd.alwyal.coba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
